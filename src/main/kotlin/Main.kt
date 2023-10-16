f31624fc17b8ddce4ffe229305781eec7dc821b1
malik <malik.wachter@protonmail.com>
2023-10-16 19:48:13 +0200

    Removed unused var

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -34,7 +34,6 @@ fun multiplayer() {
     var playerTwoField2 = Point(-1,-1)
     var playerTwoField3 = Point(-1,-1)
     var playerTwoField4 = Point(-1,-1)
-    var playerTwoField5 = Point(-1,-1)
     val gameField1 = Point(1, 1)
     val gameField2 = Point(3, 1)
     val gameField3 = Point(5, 1)
@@ -46,7 +45,7 @@ fun multiplayer() {
     val gameField9 = Point(5, 5)
 
     while(again){
-        gameField(gameField1, gameField2, gameField3, gameField4, gameField5, gameField6, gameField7, gameField8, gameField9, playerOneField1, playerOneField2, playerOneField3, playerOneField4, playerOneField5, playerTwoField1, playerTwoField2, playerTwoField3, playerTwoField4, playerTwoField5)
+        gameField(gameField1, gameField2, gameField3, gameField4, gameField5, gameField6, gameField7, gameField8, gameField9, playerOneField1, playerOneField2, playerOneField3, playerOneField4, playerOneField5, playerTwoField1, playerTwoField2, playerTwoField3, playerTwoField4)
         println("Player 1: Choose your field:")
         playerInput = readlnOrNull()
 
@@ -76,7 +75,7 @@ fun multiplayer() {
         } else
             println("ERR: Input was NULL")
 
-        gameField(gameField1, gameField2, gameField3, gameField4, gameField5, gameField6, gameField7, gameField8, gameField9, playerOneField1, playerOneField2, playerOneField3, playerOneField4, playerOneField5, playerTwoField1, playerTwoField2, playerTwoField3, playerTwoField4, playerTwoField5)
+        gameField(gameField1, gameField2, gameField3, gameField4, gameField5, gameField6, gameField7, gameField8, gameField9, playerOneField1, playerOneField2, playerOneField3, playerOneField4, playerOneField5, playerTwoField1, playerTwoField2, playerTwoField3, playerTwoField4)
         println("Player 2! Choose your field: ")
         playerInput = readlnOrNull()
 
@@ -107,14 +106,14 @@ fun multiplayer() {
     }
 }
 
-fun gameField(gameField1: Point, gameField2: Point, gameField3: Point, gameField4: Point, gameField5: Point, gameField6: Point, gameField7: Point, gameField8: Point, gameField9: Point, playerOneField1: Point, playerOneField2: Point, playerOneField3: Point, playerOneField4: Point, playerOneField5: Point, playerTwoField1: Point, playerTwoField2: Point, playerTwoField3: Point, playerTwoField4: Point, playerTwoField5: Point) {
+fun gameField(gameField1: Point, gameField2: Point, gameField3: Point, gameField4: Point, gameField5: Point, gameField6: Point, gameField7: Point, gameField8: Point, gameField9: Point, playerOneField1: Point, playerOneField2: Point, playerOneField3: Point, playerOneField4: Point, playerOneField5: Point, playerTwoField1: Point, playerTwoField2: Point, playerTwoField3: Point, playerTwoField4: Point) {
 
     for (y in 1..5) {
         for (x in 1..5) {
             val p = Point(x, y)
             if(p == playerOneField1 || p == playerOneField2 || p == playerOneField3 || p == playerOneField4 || p == playerOneField5)
                 print("❌")
-            else if(p == playerTwoField1 || p == playerTwoField2 || p == playerTwoField3 || p == playerTwoField4 || p == playerTwoField5)
+            else if(p == playerTwoField1 || p == playerTwoField2 || p == playerTwoField3 || p == playerTwoField4)
                 print("⭕")
             else if (p == gameField1 || p == gameField2 || p == gameField2 || p == gameField3 || p == gameField4 || p == gameField5 || p == gameField6 || p == gameField7 || p == gameField8 || p == gameField9)
                 print("\uD83D\uDD33")
